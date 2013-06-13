6278566c48fd4997c758689f4cb99a66faa26c1e
Eric Prunier <eric.prunier@gmail.com>
2013-06-13 22:24:53 +0200

    Add basic operations

--- src/clj_ejml/core.clj
+++ src/clj_ejml/core.clj
@@ -2,10 +2,9 @@
   (:import [org.ejml.ops CommonOps]
            [org.ejml.data DenseMatrix64F]))
 
-(defn zeros
-  [rows cols]
-  (DenseMatrix64F. rows cols))
-
+;;
+;; Matrix creation
+;;
 (defn matrix
   "Create a new matrix.
 
@@ -19,9 +18,9 @@
           (map double-array)
           into-array
           DenseMatrix64F.))
-  ([data rows]
-     (let [cols (/ (count data) rows)]
-       (DenseMatrix64F. rows cols true (double-array data)))))
+  ([data num-rows]
+     (let [cols (/ (count data) num-rows)]
+       (DenseMatrix64F. num-rows cols true (double-array data)))))
 
 (defn row-vector
   "Create a row vector."
@@ -33,40 +32,126 @@
   [data]
   (matrix data (count data)))
 
-(defn set-value
-  "Set the value "
-  [M row col val]
-  (let [T (.copy M)]
-    (.set T row col val)
-    T))
+(defn zeros
+  "Create a matrix with all elements set to 0."
+  ([size]
+     (zeros (first size) (second size)))
+  ([rows cols]
+     (DenseMatrix64F. rows cols)))
+
+(defn ones
+  "Create a matrix with all elements set to 1."
+  ([size]
+     (ones (first size) (second size)))
+  ([rows cols]
+     (let [row (take cols (repeat 1))]
+       (matrix (take rows (repeat row))))))
 
 (defn eye
   "Return an identity matrix."
   [size]
   (CommonOps/identity size))
 
-(defn- with-copy
-  [M f]
-  (let [T (.copy M)]
-    (f T)
-    T))
+;;
+;; Matrix operations
+;;
+(defn size
+  "Return the number of rows and columns in matrix M."
+  [M]
+  [(.getNumRows M)
+   (.getNumCols M)])
 
+(defn set-value
+  "Set the value for the given row and col of the matrix M."
+  [M row col value]
+  (let [R (.copy M)]
+    (.set R row col value)
+    R))
 
 (defn transpose
   "Transpose the M matrix."
   [M]
-  (let [T (.copy M)]
-    (CommonOps/transpose T)
-    T))
+  (let [R (.copy M)]
+    (CommonOps/transpose R)
+    R))
 
-(defn negative
+(defn minus
+  "Change the sign of all elements."
   [M]
-  (let [T (.copy M)]
-    (CommonOps/changeSign T)
-    T))
+  (let [R (zeros (size M))]
+    (CommonOps/changeSign R)
+    R))
+
+(defn inv
+  [M]
+  (let [R (zeros (size M))]
+    (CommonOps/invert M R)
+    R))
 
 (defn pinv
+  "Computes the pseudo-inverse of the matrix."
   [M]
-  (let [T (.copy M)]
-    (CommonOps/pinv M T)
-    T))
+  (let [R (zeros (size M))]
+    (CommonOps/pinv M R)
+    R))
+
+(defn add
+  "Add matrix A and matrix B."
+  [A B]
+  (let [R (zeros (size A))]
+    (CommonOps/add A B R)
+    R))
+
+(defn sub
+  "Subtract matrix B from matrix A."
+  [A B]
+  (let [R (zeros (size A))]
+    (CommonOps/sub A B R)
+    R))
+
+(defn mul
+  "Multiply matrix A by matrix B."
+  [A B]
+  (let [result-rows (-> A size first)
+        result-cols (-> B size second)
+        R (zeros result-rows result-cols)]
+    (CommonOps/mult A B R)
+    R))
+
+(defn emul
+  "Multiply element wise matrix A by matrix B."
+  [A B]
+  (let [R (zeros (size A))]
+    (CommonOps/elementMult A B R)
+    R))
+
+(defn scale
+  "Multiply all elements of matrix M by a scalar n."
+  [M n]
+  (let [R (zeros (size M))]
+    (CommonOps/scale n M R)
+    R))
+
+(defn div
+  "Divide all elements of matrix A by a scalar n."
+  [M n]
+  (let [R (zeros (size M))]
+    (CommonOps/divide n M R)
+    R))
+
+(defn ediv
+  "Divide element wise matrix A by matrix B."
+  [A B]
+  (let [R (zeros (size A))]
+    (CommonOps/elementDiv A B R)
+    R))
+
+(defn trace
+  "Compute the trace of the matrix M."
+  [M]
+  (CommonOps/trace M))
+
+(defn det
+  "Compute the determinant of the matrix M."
+  [M]
+  (CommonOps/det M))
